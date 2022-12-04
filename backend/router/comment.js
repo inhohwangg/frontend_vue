937e36f2decf880c87405f2028f89b977b7b1575
inhohwangg <sk1440sk@naver.com>
2022-12-04 19:37:09 +0900

    commentPost api code add

--- backend/router/comment.js
+++ backend/router/comment.js
@@ -8,7 +8,16 @@ const Post = require("../schema/post");
 // 댓글 생성 기능
 router.post("/commentPost", auth, async (req, res) => {
   try {
-    res.status(201).json({ result: "댓글 생성 완료" });
+    const { userId } = req.query;
+    const { content } = req.body;
+    let commentNum = 0;
+    let add = await Comment.find(null, { commentNum: true });
+    for (let i = 0; i < add.length; i++) {
+      if (add[i].commentNum === undefined) commentNum = 1;
+      else commentNum += 1;
+    }
+    let result = await Comment.create({ userId, content, commentNum });
+    res.status(201).json({ result: "댓글 생성 완료", result });
   } catch (error) {
     console.error(error, "commentPost Api error");
     res.status(400).json({ result: false });
